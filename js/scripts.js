5c94f9d51140539c1f05ce7ce8b60fbfce6644ef
Mateusz Lawczys <ma6tek@gmail.com>
2017-12-23 17:23:36 +0100

    Fix

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,6 @@ $(function(){
         var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
         $(element).append(button);
     });
-
     $("button").click(function(){
         alert($(this).attr("data-tmp"));
     });
